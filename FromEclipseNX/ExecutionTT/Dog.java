92a54aee54316cffaee4404c823652729f415b10
Stavros Marinis <redenicht@hotmail.com>
2018-04-27 10:22:06 +0200

    submit inheritance class

---
+++ FromEclipseNX/ExecutionTT/Dog.java
@@ -0,0 +1,27 @@
+package ExecutionTT;
+
+public class Dog{
+
+    private String name;
+    
+    
+    public Dog(String dogName) {
+        
+        name = dogName;
+        
+    }
+    
+    
+    public String getName() {
+        
+        return name;
+        
+    }
+    
+    public void setName(String aName) {
+        
+        name = aName;
+        
+    }
+    
+}
